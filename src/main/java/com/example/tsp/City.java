07fded1b3dcf24bf75d0db0678f3647263bcb6c6
Vignesh P <34863107+VigneshPerumal2@users.noreply.github.com>
2023-04-12 16:33:59 -0400

    Added city and Edge class

--- src/main/java/com/example/tsp/City.java
+++ src/main/java/com/example/tsp/City.java
@@ -1,2 +1,54 @@
-package com.example.tsp;public class City {
+package com.example.tsp;
+
+//import javafx.scene.paint.Color;
+//import javafx.scene.paint.Color;
+//import javafx.scene.shape.Circle;
+
+public class City {
+    private static int idCounter = 0;
+
+    private final int id;
+    private final double x;
+    private final double y;
+
+    public City(double x, double y) {
+        this.id = idCounter++;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof City)) {
+            return false;
+        }
+        City other = (City) obj;
+        return this.id == other.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(id);
+    }
+
+    public double distanceTo(City other) {
+        double dx = x - other.x;
+        double dy = y - other.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
 }
